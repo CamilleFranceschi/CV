97ad87f32ca0868fbee72447c50dc8887fb174a4
CamilleFranceschi <franceschi.camille@gmail.com>
2017-01-20 00:26:07 +0100

    worked on skills details

--- assets/js/hide-unhide.js
+++ assets/js/hide-unhide.js
@@ -24,9 +24,39 @@
 //     );
 // });
 
-$(document).ready(function() {
-$("#content").hide();
-$("#hideshow").click(function(){
-    $("#content").toggle(1000);
+// $(document).ready(function() {
+// $("#content").hide();
+// $("#hideshow").click(function(){
+//     $("#content").toggle(1000);
+// });
+// });
+
+// $(document).ready(function() {
+// $("#" + this.content).hide();
+// $("#hideshow").click(function(){
+//     $("#" + this.content).toggle("slow");
+// });
+// });
+// find('#content').hide();
+// $('.button').on('click', function() {
+// 	$parent_box = $(this).closest('.box');
+// 	$parent_box.siblings().find('#content').hide();
+// 	$parent_box.find('#content').toggle();
+// });
+
+$.each($(".content"),function(){
+     $(this).hide();
 });
-});
+
+// $(document).ready(function() {
+// $('.content').hide();
+// $('.button').on('click', function() {
+// 	$parent_box = $(this).closest('.box');
+// 	$parent_box.siblings().find('.content').hide();
+// 	$parent_box.find('.content').toggle(1000, 'swing');
+// });
+// });
+
+// $(".buttons").click(function () {
+//   $("#" + this.value).toggle("slow");
+// });
